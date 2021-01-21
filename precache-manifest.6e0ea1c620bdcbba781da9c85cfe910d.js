7927d1ed04bcdf6793f6d39e47cbf8bc4dbe8ed2
jatin Chopra <jatinchopra@jatins-MacBook-Pro.local>
2021-01-22 03:49:41 +0530

    Updates

---
+++ precache-manifest.6e0ea1c620bdcbba781da9c85cfe910d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220d1d8bae7ca3cdef73f2d617619ba3",
+    "url": "/GiphySearch/index.html"
+  },
+  {
+    "revision": "295270c58a136747233d",
+    "url": "/GiphySearch/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "817bb9497ea1590db368",
+    "url": "/GiphySearch/static/js/2.d8bcd81c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GiphySearch/static/js/2.d8bcd81c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "295270c58a136747233d",
+    "url": "/GiphySearch/static/js/main.16e3cf8d.chunk.js"
+  },
+  {
+    "revision": "d45247eea33b9d9f1fee",
+    "url": "/GiphySearch/static/js/runtime-main.48c1f57b.js"
+  }
+]);
